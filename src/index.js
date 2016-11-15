171b529aa40d8cdc4594797754c06f0342240049
Andrey Lebedev <lebedancer@gmail.com>
2016-11-15 16:01:34 +0300

    7. Create Header

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, browserHistory } from 'react-dom';
+import { Router, browserHistory } from 'react-router';
 import routes from './routes';
-import './styles/styles/css'
+import './styles/styles.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
-
-export default (
+render(
    <Router history={browserHistory} routes={routes}/>,
     document.getElementById('app')
 );
