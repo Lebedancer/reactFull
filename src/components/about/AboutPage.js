73d92c2c50e6328c903cd4e13dcfbe96a8bf2440
Andrey Lebedev <lebedancer@gmail.com>
2016-11-15 14:27:42 +0300

    5. Update Entry Point

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>About</h1>
+                <p>About AboutAboutReact, Reduxssdfdsfsdfsdfsdfdsf</p>
+            </div>
+        )
+    }
+}
+
+export default AboutPage;
