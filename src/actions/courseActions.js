ee21c9875afc67a6db74d53b5b4c32e42fdc2573
Andrey Lebedev <lebedancer@gmail.com>
2016-12-05 22:01:35 +0300

    11.10

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -5,11 +5,31 @@ export function loadCoursesSuccess(courses) {
     return { type: types.LOAD_COURSES_SUCCESS, courses };
 }
 
+export function updateCourseSuccess(courses) {
+    return { type: types.UPDATE_COURSES_SUCCESS, courses };
+}
+
+export function createCoursesSuccess(courses) {
+    return { type: types.CREATE_COURSES_SUCCESS, courses };
+}
+
 export function loadCourses() {
     return function(dispatch) {
         return courseApi.getAllCourses()
             .then(courses => {
                 dispatch(loadCoursesSuccess(courses));
+            })
+            .catch(error => {
+                throw(error);
+            });
+    };
+}
+
+export function saveCourse() {
+    return function(dispatch) {
+        return courseApi.saveCourse()
+            .then(course => {
+                course.is ? dispatch(updateCourseSuccess(course)) : dispatch(createCourseSuccess(course));
             })
             .catch(error => {
                 throw(error);
