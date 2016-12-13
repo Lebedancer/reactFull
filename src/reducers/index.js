732423f29cd587da4b70a34dac5dfa40d6db143b
Andrey Lebedev <lebedancer@gmail.com>
2016-12-13 16:22:25 +0300

    12.4

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import courseReducer from './courseReducer';
 import authorReducer from './authorReducer';
+import ajaxStatusReducer from './ajaxStatusReducer';
 
 const rootReducer = combineReducers({
     courseReducer,
-    authorReducer
+    authorReducer,
+    ajaxStatusReducer
 });
 
 export default rootReducer;
